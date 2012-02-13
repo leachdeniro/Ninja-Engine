1fdb2a810e5a853c09ec63d1a44601f5f5b69f44
Luis Astudillo <leachdeniro@gmail.com>
2012-02-13 00:15:00 -0300

    agrego funcionalidad de movimiento del personaje al llegar al límite del escenario. No está terminado, es un ejemplo de implementeación. Verlo con depuración de java script

--- Character.js
+++ Character.js
@@ -1,102 +1,1 @@
-
-/*
-Propiedades sprite de prueba
-width = 196
-height = 168
-widthFrame = 28
-heightFrame = 42
-
- */
-
-function Character(src, x, y, z, widthFrame, heightFrame, numRows, numColums, velocity, update)
-{
-   // Constructor de la clase padre
-   Entity.call(this, x, y, z, update);
-   this.image = new Image();
-   this.image.src = src;
-   this.widthFrame = widthFrame;
-   this.heightFrame = heightFrame;
-   this.numRows = numRows;
-   this.numColums = numColums;
-
-   // posición inicial
-   this.xPositionOnSprite = 0;
-   this.yPositionOnSprite = 84;
-
-   // variables para controlar velocidad del personaje
-   this.velocityFactor = velocity;
-   this.velocity = velocity;
-
-   // máximos del Sprite
-   this.maxWitdhtSprite = widthFrame * numColums - widthFrame;
-   this.maxHeighttSprite = heightFrame * numRows - heightFrame;
-
-
-   this.draw = function (context)
-   {
-
-      context.drawImage(this.image, this.xPositionOnSprite, this.yPositionOnSprite, this.widthFrame, this.heightFrame, this.x, this.y, this.widthFrame + 20, this.heightFrame + 20);
-
-   }
-
-   this.update = function ()
-   {
-      this.move(direction_scrolling);
-   }
-
-   /* se desplaza por una imagen
-   */
-   this.move = function(action)
-   {
-      switch (action)
-      {
-         case NORTH :
-            this.animationSprite();
-            this.yPositionOnSprite = 42;
-
-            break;
-         case EAST :
-            this.animationSprite();
-            this.yPositionOnSprite = 84;
-
-            break;
-         case SOUTH :
-            this.animationSprite();
-            this.yPositionOnSprite = 0;
-
-            break;
-         case WEST :
-            this.animationSprite();
-            this.yPositionOnSprite = 126;
-
-            break;
-         default :
-            break;
-      }
-
-   }
-
-   this.animationSprite = function()
-   {
-
-      if (this.velocityFactor % this.velocity == 0)
-      {
-         if ((this.xPositionOnSprite + this.widthFrame) <= (this.maxWitdhtSprite))
-         {
-            this.xPositionOnSprite += this.widthFrame;
-         }
-         else
-         {
-            this.xPositionOnSprite = 0;
-         }
-         this.velocityFactor = 1;
-      }
-      else
-      {
-         this.velocityFactor += 1;
-      }
-   }
-}
-
-// Extiendo de Entity
-Character.prototype = new Entity();
+/*Propiedades sprite de pruebawidth = 196height = 168widthFrame = 28heightFrame = 42 */function Character(src, x, y, z, widthFrame, heightFrame, numRows, numColums, velocity, xsd,ysd,widthStage, heightStage,update){   // Constructor de la clase padre   Entity.call(this, x, y, z, update);       this.xInit = x;    this.yInit = y;      this.image = new Image();   this.image.src = src;   this.widthFrame = widthFrame;   this.heightFrame = heightFrame;   this.numRows = numRows;   this.numColums = numColums;   // posición inicial   this.xPositionOnSprite = 0;   this.yPositionOnSprite = 84;   // variables para controlar velocidad del personaje   this.velocityFactor = velocity;   this.velocity = velocity;   // máximos del Sprite   this.maxWitdhtSprite = widthFrame * numColums - widthFrame;   this.maxHeighttSprite = heightFrame * numRows - heightFrame;      //m‡ximos ancho y alto   this.maxWStage = widthStage - widthFrame;    this.maxHStage = heightStage - heightFrame;         //desplazamiento de los ejex x y x para el personaje    this.xsd = xsd;    this.ysd = ysd;   this.draw = function (context)   {      context.drawImage(this.image, this.xPositionOnSprite, this.yPositionOnSprite, this.widthFrame, this.heightFrame, this.x, this.y, this.widthFrame + 20, this.heightFrame + 20);   }   this.update = function ()   {      this.move(direction_scrolling);   }   /* se desplaza por una imagen   */   this.move = function(action)   {      switch (action)      {         case NORTH :            this.animationSprite();            this.yPositionOnSprite = 42;            break;         case EAST :            this.animationSprite();            this.yPositionOnSprite = 84;            break;         case SOUTH :            this.animationSprite();            this.yPositionOnSprite = 0;            break;         case WEST :            this.animationSprite();            this.yPositionOnSprite = 126;            break;         default :            break;      }      if (limitStage !== NONE){        this.advance(action);        if (this.x == this.xInit){            limitStage = NONE;        }    }    }   this.animationSprite = function()   {      if (this.velocityFactor % this.velocity == 0)      {         if ((this.xPositionOnSprite + this.widthFrame) <= (this.maxWitdhtSprite))         {            this.xPositionOnSprite += this.widthFrame;         }         else         {            this.xPositionOnSprite = 0;         }         this.velocityFactor = 1;      }      else      {         this.velocityFactor += 1;      }   }this.advance = function(direction)   {      switch (direction)      {         case NORTH :            if ((this.y - this.ysd) >= 0)            {               this.y -= this.ysd;            }            else            {               this.y = 0;             }            break;         case EAST :            if ((this.x + this.xsd) <= (this.maxWStage - this.xsd))            {               this.x += this.ysd;            }            else            {               this.x = this.maxWStage;                           }            break;         case SOUTH :            if ((this.y + this.ysd) <= (this.maxHStage - this.ysd))            {               this.y += this.ysd;            }            else            {               this.y = this.maxHStage;            }            break;         case WEST :            if ((this.x - this.xsd) >= 0)            {               this.x -= this.xsd;            }            else            {               this.x = 0;            }            break;         default :            break;      }   }}// Extiendo de EntityCharacter.prototype = new Entity();
--- EntityImage.js
+++ EntityImage.js
@@ -1,75 +1,1 @@
-/* 
-    La variable direction_scrolling, es externa porque responde a un evento externo al objeto. Los valores que soporta están definidos
-    en la clase EntiyImageBase valores(NORTH,EAST,SOUTH,WEST,NONE).
-*/
-function EntityImage(src, x, y, z, widthScroll, heightScroll, xs, ys, xsd, ysd, widthStage, heightStage, update)
-{
-   // Constructor de la clase padre
-   EntityImageBase.call(this, src, x, y, z,widthScroll, heightScroll, xs, ys, xsd, ysd, widthStage, heightStage, update);
-  
-   this.update = function ()
-   {
-      this.move();
-   }
-
-   /* se desplaza por una imagen
-   */
-   this.move = function()
-   {
-
-      switch (direction_scrolling)
-      {
-         case NORTH :
-            if ((this.yScroll - this.yAxisd) >= 0)
-            {
-               this.yScroll -= this.yAxisd;
-            }
-            else
-            {
-               this.yScroll = 0;
-            }
-
-            break;
-         case EAST :
-            if ((this.xScroll + this.xAxisd) <= (this.maxWStage - this.xAxisd))
-            {
-               this.xScroll += this.xAxisd;
-            }
-            else
-            {
-               this.xScroll = this.maxWStage;
-            }
-
-            break;
-         case SOUTH :
-            if ((this.yScroll + this.yAxisd) <= (this.maxHStage - this.yAxisd))
-            {
-               this.yScroll += this.yAxisd;
-            }
-            else
-            {
-               this.yScroll = this.maxHStage;
-            }
-
-            break;
-         case WEST :
-            if ((this.xScroll - this.xAxisd) >= 0)
-            {
-               this.xScroll -= this.xAxisd;
-            }
-            else
-            {
-               this.xScroll = 0;
-            }
-
-            break;
-         default :
-            break;
-      }
-
-   }
-
-}
-
-// Extiendo de Entity
-EntityImage.prototype = new EntityImageBase();
+/*     La variable direction_scrolling, es externa porque responde a un evento externo al objeto. Los valores que soporta están definidos    en la clase EntiyImageBase valores(NORTH,EAST,SOUTH,WEST,NONE).*/function EntityImage(src, x, y, z, widthScroll, heightScroll, xs, ys, xsd, ysd, widthStage, heightStage, update){   // Constructor de la clase padre   EntityImageBase.call(this, src, x, y, z,widthScroll, heightScroll, xs, ys, xsd, ysd, widthStage, heightStage, update);     this.update = function ()   {      if (limitStage == NONE){        this.move();      }   }   /* se desplaza por una imagen   */   this.move = function()   {      switch (direction_scrolling)      {         case NORTH :            if ((this.yScroll - this.yAxisd) >= 0)            {               this.yScroll -= this.yAxisd;            }            else            {               this.yScroll = 0;               limitStage = NORTH;            }            break;         case EAST :            if ((this.xScroll + this.xAxisd) <= (this.maxWStage - this.xAxisd))            {               this.xScroll += this.xAxisd;            }            else            {               this.xScroll = this.maxWStage;               limitStage = EAST;            }            break;         case SOUTH :            if ((this.yScroll + this.yAxisd) <= (this.maxHStage - this.yAxisd))            {               this.yScroll += this.yAxisd;            }            else            {               this.yScroll = this.maxHStage;               limitStage = SOUTH;            }            break;         case WEST :            if ((this.xScroll - this.xAxisd) >= 0)            {               this.xScroll -= this.xAxisd;            }            else            {               this.xScroll = 0;               limitStage = WEST;            }            break;         default :            break;      }   }}// Extiendo de EntityEntityImage.prototype = new EntityImageBase();
